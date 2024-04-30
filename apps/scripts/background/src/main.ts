1fe8a6ebb2e8f52ac7265de32154ef89e6cd59ed
layinka <xcelsis02@gmail.com>
2024-04-30 13:38:37 +0100

    Initial commit

---
+++ apps/scripts/background/src/main.ts
@@ -0,0 +1,23 @@
+console.log('chrome-extension-starter - scripts-background');
+
+// chrome.identity.getAuthToken({ 'interactive': true }, getToken);
+
+// function getToken(token: any) {
+//      console.log('this is the token: ', token);
+//   let init = {
+//     method: 'GET',
+//     async: true,
+//     headers: {
+//       Authorization: 'Bearer ' + token,
+//       'Content-Type': 'application/json'
+//     },
+//     'contentType': 'json'
+//   };
+//   fetch(
+//       'https://sheets.googleapis.com/v4/spreadsheets/{spreadsheetId}/values/{range}',
+//       init)
+//       .then((response) => response.json())
+//       .then(function(data) {
+//         console.log(data)
+//       });
+// }
